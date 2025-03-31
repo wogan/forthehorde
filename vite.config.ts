c37a9be3b2a5b5a1d7a3b7fa60f4974d4f366bf0
Greg Wogan-Browne <wogan@wogan.id.au>
2025-03-31 16:03:42 +1100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { triggerkit } from 'vite-plugin-triggerkit';
+import { triggerkit } from 'triggerkit';
 
 export default defineConfig({
 	plugins: [
